f8c5befa6d41584085d1db99dd6dc6ff0c9f1d46
vampez-254 <wakandafrank@gmail.com>
2024-02-21 16:54:02 +0300

    This are java comments

---
+++ Comments.java
@@ -0,0 +1,17 @@
+public class Comments {
+    public static void main(String[]args) {
+        /*comments in java
+        * We have three types of comments in java
+        * 1. C++ Style comments -> //
+        * 2. c style comments
+        * 3. special javadoc comments */
+       //1. c++ comments style
+        System.out.println("we use// for c++ comments style");
+        /*2. c comment style */
+        System.out.println("we use /**/ for c comments style");
+        /**
+         * 3.special javadoc comment
+         */
+        System.out.println("3.we use /** */ for special javadoc comments");
+    }
+}
--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
     public static void main(String[] args){
-System.out.println("Hello Git and Github");
-    } 
+        System.out.println("Hello Git and Github");
+        System.out.println("This is a new added in InteliJ");
+    }
 }
